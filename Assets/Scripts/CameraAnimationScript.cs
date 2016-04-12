fc782a0c1d4e7d90da5b91e057fb2b1e7bbb87a4
unknown <tuukka.muukkonen@peli01pc.opisto.hkro>
2016-04-12 11:59:22 +0300

    Camera animation and script, 2D sprite animation, UIManager, old lightmaps removed

---
+++ Assets/Scripts/CameraAnimationScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAnimationScript : MonoBehaviour {
+
+	public Transform focus;
+	Animator cameraAnim;
+	public float smooth;
+
+	bool playAnimation;
+
+	void Start () {
+		cameraAnim = GetComponent<Animator>();
+	}
+
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			playAnimation = true;
+			cameraAnim.SetBool("Start", true);
+		}
+
+		if (playAnimation == true) {
+			Quaternion lookRot = Quaternion.LookRotation(focus.position - transform.position);
+			transform.rotation = Quaternion.Lerp(transform.rotation, lookRot, Time.deltaTime * smooth);
+		}
+	}
+
+	// Called at the of the animation
+	public void SetStartFalse () {
+		cameraAnim.SetBool("Start", false);
+		playAnimation = false;
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	Text text;
+
+	void Start () {
+		text = GameObject.Find("Text").GetComponent<Text>();
+	}
+
+	void Update () {
+		if (Input.inputString != "") {
+			text.text = Input.inputString;
+		}
+	}
+}
