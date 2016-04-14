427524ce6221e6cb90341e8bccb508baaffcd229
unknown <tuukka.muukkonen@peli01pc.opisto.hkro>
2016-04-14 11:19:55 +0300

    Animation switch

---
+++ Assets/AnimationSwitch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationSwitch : MonoBehaviour {
+
+	public GameObject cube;
+
+	public AnimationClip clipOne;
+	public AnimationClip clipTwo;
+
+	Animator anim;
+
+	void Start () {
+		anim = cube.GetComponent<Animator>();
+	}
+
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			anim.Play(clipOne.name);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha2)) {
+			anim.Play(clipTwo.name);
+		}
+
+	}
+}
