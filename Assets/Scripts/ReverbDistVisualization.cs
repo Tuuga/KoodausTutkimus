fe946517226b1090d5c92373ccc82f2277aa359d
unknown <tuukka.muukkonen@peli01pc.opisto.hkro>
2016-04-20 16:20:40 +0300

    Audio scene setup, UI button notes

---
+++ Assets/Scripts/ReverbDistVisualization.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ReverbDistVisualization : MonoBehaviour {
+
+    public AudioReverbZone reverbZone;
+    public Transform visualMinDist;
+    public Transform visualMaxDist;
+
+    void Update () {
+
+        float minDist = reverbZone.minDistance;
+        float maxDist = reverbZone.maxDistance;
+
+        Vector3 minDistScale = Vector3.one * minDist * 2;
+        Vector3 maxDistScale = Vector3.one * maxDist * 2;
+
+        visualMinDist.localScale = minDistScale;
+        visualMaxDist.localScale = maxDistScale;
+    }
+}
